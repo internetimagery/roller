8c38789aaa246ba2e2cb2ed3a7731c2da5961e0e
Jason Dixon <jdixon@wetafx.co.nz>
2017-09-21 18:41:18 +1200

    adding node creation

--- __init__.py
+++ __init__.py
@@ -2,11 +2,27 @@
 import math
 import maya.cmds as cmds
 
-RADIANS = math.radians(1)
 
 def main():
     err = cmds.undoInfo(openChunk=True)
     try:
+        # Create our objects. Parent them together.
+        root = cmds.group(name="roller_root", empty=True)
+        ctrl = cmds.group(name="%s_ctrl" % root, empty=True) # TODO: change this to something nicer
+        cmds.parent(ctrl, root)
+
+        # Create our nodes.
+        mult_radians = cmds.shadingNode(asUtility="multiplyDivide")
+        mult_scale = cmds.shadingNode(asUtility="multiplyDivide")
+        mult_invert = cmds.shadingNode(asUtility="multiplyDivide")
+
+        # Set values
+        radians = math.radians(1)
+        cmds.setAttr("%s.input1X" % mult_radians, radians)
+        cmds.setAttr("%s.input1Y" % mult_radians, radians)
+        cmds.setAttr("%s.input1Z" % mult_radians, radians)
+
+
         pass
     except err:
         pass
